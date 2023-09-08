2dcf6d8c354054c8e0a2dc8d2de4b65c612767c8
sakthidaran3112 <sakthidaran3112@gmail.com>
2023-09-08 22:58:51 +0200

    Edited the code to find current location

--- app/src/main/java/com/example/navigationapp/MapsActivity.kt
+++ app/src/main/java/com/example/navigationapp/MapsActivity.kt
@@ -9,14 +9,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.navigationapp.databinding.ActivityMapsBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import java.util.Locale
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
